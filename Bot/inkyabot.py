a0b14b990a45801ddb2a6d1b5219e19a0b8045a9
sho2004 <52592597+sho2004@users.noreply.github.com>
2019-10-10 20:18:59 +0900

    Add: ミュージックプレイヤーの作成

--- Bot/inkyabot.py
+++ Bot/inkyabot.py
@@ -1,16 +1,21 @@
 import discord
 
 import commands as cmd
+import musicplayer as mp
 
 TOKEN = '***********************************************************'
 CHANNEL_ID = 000000000000000000
+VOICE_CHANNEL_ID = 000000000000000000
 
 client = discord.Client()
+music = None
 
 @client.event
 async def on_ready():
     global music
     channel = client.get_channel(CHANNEL_ID)
+    voice_channel = client.get_channel(VOICE_CHANNEL_ID)
+    music = mp.MusicPlayer(voice_channel, channel)
     await channel.send('陰キャが帰ってきました\n陰キャBotについて知りたいときは.helpと入力してください')
 
 @client.event
@@ -21,6 +26,10 @@ async def on_message(message):
     # コマンド呼び出し
     if message.content[:1] == '.':
         await cmd.command_output(client, message) 
+
+    # ミュージックプレイヤーの起動
+    if message.content[:1] == '!':
+        await music.music_player(message)
 
     # あいさつ
     if message.content == 'こんにちは':
